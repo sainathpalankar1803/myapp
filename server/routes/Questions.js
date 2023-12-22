947680bffcb28810200a98db3f0d51af8447ee9e
sainathpalankar1803 <139047445+sainathpalankar1803@users.noreply.github.com>
2023-12-22 22:07:17 +0530

    Delete server directory

--- server/routes/Questions.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { AskQuestion, getAllQuestions } from "../controllers/Questions.js";
-
-const router = express.Router();
-
-router.post("/Ask", AskQuestion);
-router.get("/get", getAllQuestions);
-
-export default router;
